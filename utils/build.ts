f75ba84608824834a9e7c08d55908ab801bfd848
Gary Moore <madmangaz@gmail.com>
2022-03-13 21:14:16 +0000

    add todo to remove deprecated function

--- utils/build.ts
+++ utils/build.ts
@@ -73,6 +73,7 @@ async function build(remote: string) {
   const configPath = "./.cargo/config.toml";
   const appName = getAppName("./Cargo.toml") ?? "app";
   const appBinary = `./target/aarch64-unknown-linux-musl/release/${appName}`;
+  // TODO: Find a way to avoid using this deprecated function.
   if (!existsSync(configPath)) {
     createCargoConfig(configPath);
   }
